757ab6b25647b5c2eb8d71215cc0b4c663e106bc
wooram <zzzwwwrrr66@gmail.com>
2022-01-12 04:23:17 +0900

    <update> 0112

--- src/Shorcut/CommonHook.tsx
+++ src/Shorcut/CommonHook.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useRef, useCallback } from "react"
+import CommonProps from "./commonProps";
+
+// 값뒤의 ! 는 값을 확신할때 사용 
+// 값뒤의 ? 는 값을 확신할수 없을때 사용
 
 export default function CommonHook() {
   
   const [value, setValue] = useState('');
   const [result, setResult] = useState('');
   const inputEl = useRef<HTMLInputElement>(null);
+  // function 같은거 설정할때 useRef ex: setTimeout() 같은거 넣을때
+  const timeout = useRef<number | null>(null);
+  // typescript 가 node의setTimeout인지 window의 setTimeout인지 헷갈리니까 명확히 적어준다
+  timeout.current = window.setTimeout(() => {
+  },2000); 
 
   // onChange Evnet
   const inputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -42,6 +51,7 @@ export default function CommonHook() {
     <input type='submit'/>
     </form>
     <p>{result}</p>
+    <CommonProps text='asd' />
     </>
   )
 }
